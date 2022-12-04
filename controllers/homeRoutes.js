805b13f495ec0262546e0260e518d9c6b397bb11
Tracy Pham <phamtracy95@gmail.com>
2022-12-03 23:10:08 -0800

    Add post handelbars

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -41,7 +41,7 @@ router.get("/post/:id", async (req, res) => {
 
     const post = postData.get({ plain: true });
 
-    res.render("dashboard", {
+    res.render("post", {
       ...post,
       logged_in: req.session.logged_in,
     });
